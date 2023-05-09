71f3b99d93512d4aa804e3bfbffff3c1b80e02c3
Yiran Zhou <yiranz1@Yirans-MBP.wv.cc.cmu.edu>
2023-05-08 22:53:35 -0500

    Initial commit

---
+++ src/main/java/com/example/imageai/ImageAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.imageai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imageai/ImageAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imageai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
